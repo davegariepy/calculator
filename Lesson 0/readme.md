c320676fb19fd38dd347e64765f8a6ec90922dfe
Dave <3921481+davegariepy@users.noreply.github.com>
2026-01-08 14:01:14 -0500

    lesson 0

---
+++ Lesson 0/readme.md
@@ -0,0 +1,61 @@
+1. setup tools: vs code, chrome browser
+2. html overview
+
+- elements and tags
+- doctype
+- html
+- p with "hello world"
+
+3. head section
+
+- title
+- meta utf-8
+- link to stylesheet
+- add hello class .hello color red, add class to p tag
+
+4.  browser resets
+
+- global css reset
+- meta viewport for mobile
+
+5. javascript
+
+- create script.js file
+- add script tag to body
+- reload page
+
+6. button click to call javascript
+
+- add defer to script tag
+  Download this file in parallel with HTML
+  Do not execute it yet
+  Wait until the HTML is fully parsed
+  Then execute it, in order
+
+- add get element by id and event listener javascript code
+
+7. input element
+
+- add input element with id nameInput
+- add button element with id showName
+- add name input and name button element id
+- add name input and name button event listeners
+
+8. strings vs numbers
+
+- A
+- add input and button for number
+- same js
+- click button we see the string number
+- B
+- add second number input and button
+- add them together
+- we have a bug! the numbers are concatenated as strings
+- we need to convert them
+- strings vs numbers data types
+- C
+- convert input values to numbers using the built-in Number function
+
+9. console.log
+
+- “console.log prints values to the browser’s developer console so we can see what JavaScript is actually doing.”
---
+++ Lesson 0/script.js
@@ -0,0 +1,62 @@
+// 1
+// alert("hello world")
+
+//2
+const button = document.getElementById("alertButton");
+
+button.addEventListener("click", () => {
+  alert("hello world")
+});
+
+// 3
+const nameInput = document.getElementById("nameInput");
+const nameButton = document.getElementById("nameButton");
+
+nameButton.addEventListener("click", () => {
+  alert(nameInput.value)
+})
+
+//8A
+// const numberInput = document.getElementById("numberInput");
+// const numberButton = document.getElementById("numberButton");
+
+// numberButton.addEventListener("click", () => {
+//   alert(numberInput.value)
+// })
+
+//8B adding strings
+// const numberInputA = document.getElementById("numberInputA");
+// const numberInputB = document.getElementById("numberInputB");
+// const numberButtonAdd = document.getElementById("numberButtonAdd");
+
+// numberButtonAdd.addEventListener("click", () => {
+//   alert(numberInputA.value + numberInputB.value)
+// }) 
+
+//8C convert inputs to numbers
+// const numberInputA = document.getElementById("numberInputA");
+// const numberInputB = document.getElementById("numberInputB");
+// const numberButtonAdd = document.getElementById("numberButtonAdd");
+
+// numberButtonAdd.addEventListener("click", () => {
+//   const numA = Number(numberInputA.value);
+//   const numB = Number(numberInputB.value);
+//   alert(numA + numB)
+// })
+
+//8D console.log
+const numberInputA = document.getElementById("numberInputA");
+const numberInputB = document.getElementById("numberInputB");
+const numberButtonAdd = document.getElementById("numberButtonAdd");
+
+numberButtonAdd.addEventListener("click", () => {
+  const numA = Number(numberInputA.value);
+  const numB = Number(numberInputB.value);
+  console.log("hello console message");
+  console.log(numberInputA.value)
+  console.log(typeof numberInputA.value)
+  console.log(typeof numA)
+  alert(numA + numB)
+})
+
+
